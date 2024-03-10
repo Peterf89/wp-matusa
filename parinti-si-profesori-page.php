efb1a7fc6dd0bcacee86715c6d5cd60f6a839c1c
Peter <apfeddema@gmail.com>
2024-03-10 17:53:20 +0100

    Add working templates for the 5 pages

---
+++ parinti-si-profesori-page.php
@@ -0,0 +1,46 @@
+<?php
+    /**
+    * Template Name: Parinti si profesori
+    */
+?>
+
+<?php get_header(); ?>
+
+<div id="primary">
+	<main id="main">
+		<?php if ( have_posts() ) : ?>
+			<div class="divider"></div>
+
+			<?php
+				$term_id = get_term_by('slug', 'parinti-si-profesori', 'category')->term_id;
+				$args = array( 'category' => $term_id, 'post_type' =>  'post', 'numberposts' => 100, ); 
+				$postslist = get_posts( $args ); 
+				foreach ($postslist as $post) :
+			?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="inside-article">
+					<div class="article-holder">
+						<div id="primary">
+							<h2><?php the_title(); ?></h2> 
+							<?php the_content(); ?>
+						</div>
+					</div>
+				</div>
+			</article>
+			
+			<?php endforeach; 
+
+			else :
+
+				get_template_part( 'no-results', 'archive' );
+
+			endif;
+
+			?>
+			<div class="clear"></div>
+		</main><!-- #main -->
+		</div>
+	</div><!-- #primary -->
+	<?php
+	get_footer();
---
+++ poezii-page.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+    * Template Name: Poezii Page
+    */
+?>
+
+<?php get_header(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<?php
+				$term_id = get_term_by('name', 'poezii', 'category')->term_id;
+				$termchildren = get_term_children( $term_id, 'category' );
+				$children = implode (",", $termchildren);
+				echo do_shortcode('[pci-cat-grid term_id="' . $children . '"]');
+			?> 
+		</main>
+	</div>
+<?php
+get_footer();
