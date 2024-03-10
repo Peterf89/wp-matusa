ed08cf0914ade7d5a5c2c433b9c3ece467584c32
Peter <apfeddema@gmail.com>
2024-03-10 17:53:01 +0100

    Fix names

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action( 'wp_enqueue_scripts', 'educativ_creativ_enqueue_styles' );
 
 function educativ_creativ_enqueue_styles() {
 	wp_enqueue_style( 
-		'educativ-creativ-style',
+		'educativ_creativ-style',
 		get_parent_theme_file_uri( 'style.css' )
 	);
 }
