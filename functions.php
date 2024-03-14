86c2797d41dd46a174870f66a0d0fa512401e453
Peter <apfeddema@gmail.com>
2024-03-14 21:39:01 +0100

    Fixed frontpage, added chalkboard styling

--- functions.php
+++ functions.php
@@ -9,4 +9,23 @@ function educativ_creativ_enqueue_styles() {
 	);
 }
 
+	/**
+	 * Site content codes
+	 *
+	 * @since Kids Education 0.1
+	 *
+	 */
+	function kids_education_content_start() {
+		if( is_front_page() && !is_home() ){
+			$content_enabled = kids_education_check_top_section();
+			$content_class   = !empty( $content_enabled ) ? $content_enabled : '';
+		} else{
+			$content_class = 'site-content';
+		}
+	?>
+		<div id="content" class="<?php echo esc_attr( $content_class ); ?>">
+		<?php
+	}
+add_action( 'kids_education_content_start_action', 'kids_education_content_start', 10 );
+
 ?>
